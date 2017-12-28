79a00d436055199ec4eb2e7e7b3501e0d649a3c3
pedroolivagil <onion_oliva@hotmail.com>
2017-12-28 15:14:40 +0100

    Icons

--- mobile/src/main/java/com/olivadevelop/rolermaster/MainActivity.java
+++ mobile/src/main/java/com/olivadevelop/rolermaster/MainActivity.java
@@ -80,18 +80,12 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
+        if (id == R.id.nav_profile) {
+        } else if (id == R.id.nav_games) {
+        } else if (id == R.id.nav_trash) {
         } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+        } else if (id == R.id.nav_logout) {
+        } else if (id == R.id.nav_exit) {
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
