d49d26ca0be11145c0fac6a205350c1f75d92a52
pedroolivagil <onion_oliva@hotmail.com>
2017-12-29 01:10:31 +0100

    Icons

--- mobile/src/main/java/com/olivadevelop/rolermaster/MainActivity.java
+++ mobile/src/main/java/com/olivadevelop/rolermaster/MainActivity.java
@@ -13,40 +13,31 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+import com.olivadevelop.rolermaster.tools.AdsAdMob;
+
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+    private DrawerLayout drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.addDrawerListener(toggle);
-        toggle.syncState();
-
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
+        // Inicilizamos la publicidad
+        AdsAdMob.getInstance().initialize(this);
+        // Inicializamos el menú lateral izquierdo
+        navMenuLeft();
+        // Inicializamos el boón flotante
+        floatingActionButton();
     }
 
     @Override
     public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
+        if (getDrawer().isDrawerOpen(GravityCompat.START)) {
+            getDrawer().closeDrawer(GravityCompat.START);
         } else {
             super.onBackPressed();
         }
@@ -88,8 +79,38 @@ public class MainActivity extends AppCompatActivity
         } else if (id == R.id.nav_exit) {
         }
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
+        getDrawer().closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    public DrawerLayout getDrawer() {
+        if (drawer == null) {
+            drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        }
+        return drawer;
+    }
+
+    private void floatingActionButton() {
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    private void navMenuLeft() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, getDrawer(), toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        getDrawer().addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
     }
 }
---
+++ mobile/src/main/java/com/olivadevelop/rolermaster/tools/AdsAdMob.java
@@ -0,0 +1,66 @@
+package com.olivadevelop.rolermaster.tools;
+
+import android.content.Context;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.InterstitialAd;
+import com.google.android.gms.ads.MobileAds;
+import com.olivadevelop.rolermaster.R;
+
+
+/**
+ * Created by OlivaDevelop on 20/03/2015.
+ */
+public class AdsAdMob {
+
+    private static AdsAdMob instance;
+    private Context context;
+    private InterstitialAd interstitial;
+
+    public static AdsAdMob getInstance() {
+        if (instance == null) {
+            instance = new AdsAdMob();
+        }
+        return instance;
+    }
+
+    public void initialize(Context c) {
+        setContext(c);
+    }
+
+    private Context getContext() {
+        return context;
+    }
+
+    private void setContext(Context context) {
+        this.context = context;
+    }
+
+    public void loadIntersicial() {
+        MobileAds.initialize(getContext(), getContext().getString(R.string.id_publisher_ads));
+        // Create the interstitial.
+        interstitial = new InterstitialAd(getContext());
+        interstitial.setAdUnitId(getContext().getString(R.string.interstitial_ad_unit_id));
+        // Create ad request.
+        AdRequest adRequest2 = new AdRequest.Builder().build();
+        // Begin loading your interstitial.
+        interstitial.loadAd(adRequest2);
+    }
+
+    public void printIntersicial() {
+        if (interstitial == null) {
+            loadIntersicial();
+        } else {
+            if (interstitial.isLoaded()) {
+                interstitial.show();
+            }
+        }
+    }
+
+    public void printBanner(AdView mAdView) {
+        MobileAds.initialize(getContext(), getContext().getString(R.string.id_publisher_ads));
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+    }
+}
