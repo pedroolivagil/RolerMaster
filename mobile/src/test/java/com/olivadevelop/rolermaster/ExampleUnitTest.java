1c265596a71fd01e23ed6da6d3e5b027dbbb7264
pedro oliva gil <onion_oliva@hotmail.com>
2018-01-19 14:02:23 +0100

    V.0.0.24

--- mobile/src/test/java/com/olivadevelop/rolermaster/ExampleUnitTest.java
+++ mobile/src/test/java/com/olivadevelop/rolermaster/ExampleUnitTest.java
@@ -1,14 +1,8 @@
 package com.olivadevelop.rolermaster;
 
-import com.olivadevelop.rolermaster.persistence.controllers.Controllers;
-import com.olivadevelop.rolermaster.persistence.entities.old.TestEntity;
-
-import org.json.JSONException;
-import org.json.JSONObject;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -19,18 +13,5 @@ public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
-    }
-
-    @Test
-    public void constructEntityByJson() {
-        JSONObject json = null;
-        TestEntity entity = null;
-        try {
-            json = new JSONObject("{'TestEntity':{'key':1234,'name':'Tester result'}}");
-            entity = Controllers.getInstance().getTestController().testParse(json, TestEntity.class);
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-        assertNotNull(entity);
     }
 }
