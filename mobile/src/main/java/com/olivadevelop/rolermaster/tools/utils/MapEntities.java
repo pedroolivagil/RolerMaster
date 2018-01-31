f38dffa41854599bd56a7dfe56a47223f1e8a03f
pedroolivagil <onion_oliva@hotmail.com>
2018-01-31 01:36:55 +0100

    V.0.0.29 Entities

--- mobile/src/main/java/com/olivadevelop/rolermaster/tools/utils/MapEntities.java
+++ mobile/src/main/java/com/olivadevelop/rolermaster/tools/utils/MapEntities.java
@@ -7,9 +7,21 @@ import com.olivadevelop.rolermaster.persistence.entities.GameCategory;
 import com.olivadevelop.rolermaster.persistence.entities.Gender;
 import com.olivadevelop.rolermaster.persistence.entities.Locale;
 import com.olivadevelop.rolermaster.persistence.entities.Notification;
+import com.olivadevelop.rolermaster.persistence.entities.Parameter;
 import com.olivadevelop.rolermaster.persistence.entities.Preference;
 import com.olivadevelop.rolermaster.persistence.entities.Resource;
 import com.olivadevelop.rolermaster.persistence.entities.User;
+import com.olivadevelop.rolermaster.persistence.entities.subentities.Knowledge;
+import com.olivadevelop.rolermaster.persistence.entities.subentities.Mental;
+import com.olivadevelop.rolermaster.persistence.entities.subentities.Merit;
+import com.olivadevelop.rolermaster.persistence.entities.subentities.Physical;
+import com.olivadevelop.rolermaster.persistence.entities.subentities.Power;
+import com.olivadevelop.rolermaster.persistence.entities.subentities.Social;
+import com.olivadevelop.rolermaster.persistence.entities.subentities.Talent;
+import com.olivadevelop.rolermaster.persistence.entities.subentities.Technique;
+import com.olivadevelop.rolermaster.persistence.entities.subentities.Undertone;
+import com.olivadevelop.rolermaster.persistence.entities.subentities.Virtue;
+import com.olivadevelop.rolermaster.persistence.entities.subentities.Weapon;
 import com.olivadevelop.rolermaster.persistence.pojo.GenericTrans;
 import com.olivadevelop.rolermaster.persistence.pojo.NotificationPriority;
 import com.olivadevelop.rolermaster.persistence.pojo.TypeResource;
@@ -40,6 +52,18 @@ public abstract class MapEntities {
     private static Class NOTIFICATION_PRIORITY = NotificationPriority.class;
     private static Class TYPE_RESOURCE = TypeResource.class;
     private static Class CHARACTER = Character.class;
+    private static Class PARAMETER = Parameter.class;
+    private static Class KNOWLEDGE = Knowledge.class;
+    private static Class MENTAL = Mental.class;
+    private static Class MERIT = Merit.class;
+    private static Class PHYSICAL = Physical.class;
+    private static Class POWER = Power.class;
+    private static Class SOCIAL = Social.class;
+    private static Class TALENT = Talent.class;
+    private static Class TECHNIQUE = Technique.class;
+    private static Class UNDERTONE = Undertone.class;
+    private static Class VIRTUE = Virtue.class;
+    private static Class WEAPON = Weapon.class;
 
     static Class findByString(String className) {
         Class retorno = null;
@@ -71,6 +95,30 @@ public abstract class MapEntities {
             retorno = TYPE_RESOURCE;
         } else if (CHARACTER.getSimpleName().toLowerCase().trim().equals(className.trim().toLowerCase())) {
             retorno = CHARACTER;
+        } else if (PARAMETER.getSimpleName().toLowerCase().trim().equals(className.trim().toLowerCase())) {
+            retorno = PARAMETER;
+        } else if (KNOWLEDGE.getSimpleName().toLowerCase().trim().equals(className.trim().toLowerCase())) {
+            retorno = KNOWLEDGE;
+        } else if (MENTAL.getSimpleName().toLowerCase().trim().equals(className.trim().toLowerCase())) {
+            retorno = MENTAL;
+        } else if (MERIT.getSimpleName().toLowerCase().trim().equals(className.trim().toLowerCase())) {
+            retorno = MERIT;
+        } else if (PHYSICAL.getSimpleName().toLowerCase().trim().equals(className.trim().toLowerCase())) {
+            retorno = PHYSICAL;
+        } else if (POWER.getSimpleName().toLowerCase().trim().equals(className.trim().toLowerCase())) {
+            retorno = POWER;
+        } else if (SOCIAL.getSimpleName().toLowerCase().trim().equals(className.trim().toLowerCase())) {
+            retorno = SOCIAL;
+        } else if (TALENT.getSimpleName().toLowerCase().trim().equals(className.trim().toLowerCase())) {
+            retorno = TALENT;
+        } else if (TECHNIQUE.getSimpleName().toLowerCase().trim().equals(className.trim().toLowerCase())) {
+            retorno = TECHNIQUE;
+        } else if (UNDERTONE.getSimpleName().toLowerCase().trim().equals(className.trim().toLowerCase())) {
+            retorno = UNDERTONE;
+        } else if (VIRTUE.getSimpleName().toLowerCase().trim().equals(className.trim().toLowerCase())) {
+            retorno = VIRTUE;
+        } else if (WEAPON.getSimpleName().toLowerCase().trim().equals(className.trim().toLowerCase())) {
+            retorno = WEAPON;
         }
         return retorno;
     }
