da8e090117aa328d767fe4cd6824687dcb1f82df
pedro oliva gil <onion_oliva@hotmail.com>
2018-02-26 09:58:45 +0100

    V.0.0.43 Entities Prueba

--- db/constraints.sql
+++ db/constraints.sql
@@ -1,148 +1,125 @@
 ALTER TABLE user_roler
-    ADD CONSTRAINT userroler_master_user_fk
-FOREIGN KEY (idMaster) REFERENCES user (idUser)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE;
+  ADD CONSTRAINT userroler_master_user_fk FOREIGN KEY (idMaster) REFERENCES `user` (idUser)
+  ON DELETE CASCADE
+  ON UPDATE CASCADE;
 
 ALTER TABLE user_roler
-    ADD CONSTRAINT userroler_roler_user_fk
-FOREIGN KEY (idRoler) REFERENCES user (idUser)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE;
+  ADD CONSTRAINT userroler_roler_user_fk FOREIGN KEY (idRoler) REFERENCES `user` (idUser)
+  ON DELETE CASCADE
+  ON UPDATE CASCADE;
 
 ALTER TABLE preference
-    ADD CONSTRAINT preference_user_fk
-FOREIGN KEY (idUser) REFERENCES user (idUser)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE;
+  ADD CONSTRAINT preference_user_fk FOREIGN KEY (idUser) REFERENCES `user` (idUser)
+  ON DELETE CASCADE
+  ON UPDATE CASCADE;
 
 ALTER TABLE resource
-    ADD CONSTRAINT resource_user_fk
-FOREIGN KEY (idUser) REFERENCES user (idUser)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE;
+  ADD CONSTRAINT resource_user_fk FOREIGN KEY (idUser) REFERENCES `user` (idUser)
+  ON DELETE CASCADE
+  ON UPDATE CASCADE;
 
-ALTER TABLE user
-    ADD CONSTRAINT gender_user_fk FOREIGN KEY (idGender) REFERENCES gender (idGender)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE;
+ALTER TABLE `user`
+  ADD CONSTRAINT gender_user_fk FOREIGN KEY (idGender) REFERENCES gender (idGender)
+  ON DELETE CASCADE
+  ON UPDATE CASCADE;
 
-ALTER TABLE user
-    ADD CONSTRAINT country_user_fk FOREIGN KEY (idCountry) REFERENCES country (idCountry)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE;
+ALTER TABLE `user`
+  ADD CONSTRAINT country_user_fk FOREIGN KEY (idCountry) REFERENCES country (idCountry)
+  ON DELETE CASCADE
+  ON UPDATE CASCADE;
 
 ALTER TABLE gender_trans
-    ADD CONSTRAINT gender_gendertrans_fk
-FOREIGN KEY (idGender) REFERENCES gender (idGender)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE;
+  ADD CONSTRAINT gender_gendertrans_fk FOREIGN KEY (idGender) REFERENCES gender (idGender)
+  ON DELETE CASCADE
+  ON UPDATE CASCADE;
 
 ALTER TABLE locale_trans
-    ADD CONSTRAINT locale_localetrans_fk
-FOREIGN KEY (idLocale) REFERENCES localeGroup (idLocale)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE;
+  ADD CONSTRAINT locale_localetrans_fk FOREIGN KEY (idLocale) REFERENCES locale (idLocale)
+  ON DELETE CASCADE
+  ON UPDATE CASCADE;
 
 ALTER TABLE country
-    ADD CONSTRAINT country_locale_fk
-FOREIGN KEY (idLocale) REFERENCES localeGroup (idLocale)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE;
+  ADD CONSTRAINT country_locale_fk FOREIGN KEY (idLocale) REFERENCES locale (idLocale)
+  ON DELETE CASCADE
+  ON UPDATE CASCADE;
 
 ALTER TABLE country_trans
-    ADD CONSTRAINT country_countrytrans_fk
-FOREIGN KEY (idCountry) REFERENCES country (idCountry)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE;
+  ADD CONSTRAINT country_countrytrans_fk FOREIGN KEY (idCountry) REFERENCES country (idCountry)
+  ON DELETE CASCADE
+  ON UPDATE CASCADE;
 
 ALTER TABLE country_trans
-    ADD CONSTRAINT countrytrans_locale_fk
-FOREIGN KEY (idLocale) REFERENCES localeGroup (idLocale)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE;
+  ADD CONSTRAINT countrytrans_locale_fk FOREIGN KEY (idLocale) REFERENCES locale (idLocale)
+  ON DELETE CASCADE
+  ON UPDATE CASCADE;
 
 ALTER TABLE gender_trans
-    ADD CONSTRAINT gendertrans_locale_fk
-FOREIGN KEY (idLocale) REFERENCES localeGroup (idLocale)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE;
+  ADD CONSTRAINT gendertrans_locale_fk FOREIGN KEY (idLocale) REFERENCES locale (idLocale)
+  ON DELETE CASCADE
+  ON UPDATE CASCADE;
 
 ALTER TABLE parameter
-    ADD CONSTRAINT parameter_locale_fk
-FOREIGN KEY (idLocale) REFERENCES localeGroup (idLocale)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE;
+  ADD CONSTRAINT parameter_locale_fk FOREIGN KEY (idLocale) REFERENCES locale (idLocale)
+  ON DELETE CASCADE
+  ON UPDATE CASCADE;
 
 ALTER TABLE game
-    ADD CONSTRAINT game_gamecategory_fk
-FOREIGN KEY (idCategory) REFERENCES game_category (idCategory)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE;
+  ADD CONSTRAINT game_gamecategory_fk FOREIGN KEY (idCategory) REFERENCES game_category (idCategory)
+  ON DELETE CASCADE
+  ON UPDATE CASCADE;
 
 ALTER TABLE game
-    ADD CONSTRAINT game_zone_resource_fk
-FOREIGN KEY (zoneResource) REFERENCES resource (idResource)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE;
+  ADD CONSTRAINT game_zone_resource_fk FOREIGN KEY (zoneResource) REFERENCES resource (idResource)
+  ON DELETE CASCADE
+  ON UPDATE CASCADE;
 
 ALTER TABLE game
-    ADD CONSTRAINT game_header_resource_fk
-FOREIGN KEY (headerResource) REFERENCES resource (idResource)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE;
+  ADD CONSTRAINT game_header_resource_fk FOREIGN KEY (headerResource) REFERENCES resource (idResource)
+  ON DELETE CASCADE
+  ON UPDATE CASCADE;
 
 ALTER TABLE user_game
-    ADD CONSTRAINT usergame_user_fk
-FOREIGN KEY (idUser) REFERENCES user (idUser)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE;
+  ADD CONSTRAINT usergame_user_fk FOREIGN KEY (idUser) REFERENCES `user` (idUser)
+  ON DELETE CASCADE
+  ON UPDATE CASCADE;
 
 ALTER TABLE user_game
-    ADD CONSTRAINT usergame_game_fk
-FOREIGN KEY (idGame) REFERENCES game (idGame)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE;
+  ADD CONSTRAINT usergame_game_fk FOREIGN KEY (idGame) REFERENCES game (idGame)
+  ON DELETE CASCADE
+  ON UPDATE CASCADE;
 
 ALTER TABLE game_resource
-    ADD CONSTRAINT gameresource_game_fk
-FOREIGN KEY (idGame) REFERENCES game (idGame)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE;
+  ADD CONSTRAINT gameresource_game_fk FOREIGN KEY (idGame) REFERENCES game (idGame)
+  ON DELETE CASCADE
+  ON UPDATE CASCADE;
 
 ALTER TABLE game_resource
-    ADD CONSTRAINT resource_game_fk
-FOREIGN KEY (idResource) REFERENCES resource (idResource)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE;
+  ADD CONSTRAINT resource_game_fk FOREIGN KEY (idResource) REFERENCES resource (idResource)
+  ON DELETE CASCADE
+  ON UPDATE CASCADE;
 
 ALTER TABLE game_category
-    ADD CONSTRAINT gamecategory_user_fk
-FOREIGN KEY (idUser) REFERENCES user (idUser)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE;
+  ADD CONSTRAINT gamecategory_user_fk FOREIGN KEY (idUser) REFERENCES `user` (idUser)
+  ON DELETE CASCADE
+  ON UPDATE CASCADE;
 
 ALTER TABLE character_game
-    ADD CONSTRAINT charactergame_game_fk
-FOREIGN KEY (idGame) REFERENCES game (idGame)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE;
+  ADD CONSTRAINT charactergame_game_fk FOREIGN KEY (idGame) REFERENCES game (idGame)
+  ON DELETE CASCADE
+  ON UPDATE CASCADE;
 
 ALTER TABLE character_game
-    ADD CONSTRAINT charactergame_character_fk
-FOREIGN KEY (idCharacter) REFERENCES `character` (idCharacter)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE;
+  ADD CONSTRAINT charactergame_character_fk FOREIGN KEY (idCharacter) REFERENCES `character` (idCharacter)
+  ON DELETE CASCADE
+  ON UPDATE CASCADE;
 
 ALTER TABLE character_user
-    ADD CONSTRAINT characteruser_user_fk
-FOREIGN KEY (idUser) REFERENCES user (idUser)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE;
+  ADD CONSTRAINT characteruser_user_fk FOREIGN KEY (idUser) REFERENCES `user` (idUser)
+  ON DELETE CASCADE
+  ON UPDATE CASCADE;
 
 ALTER TABLE character_user
-    ADD CONSTRAINT characteruser_character_fk
-FOREIGN KEY (idCharacter) REFERENCES `character` (idCharacter)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE;
+  ADD CONSTRAINT characteruser_character_fk FOREIGN KEY (idCharacter) REFERENCES `character` (idCharacter)
+  ON DELETE CASCADE
+  ON UPDATE CASCADE;
 
--- db/structure.sql
+++ db/structure.sql
@@ -1,191 +1,195 @@
+DROP TABLE `character_game`, `character_user`, `country_trans`, `game_resource`, `gender_trans`, `locale_trans`, `parameter`, `preference`, `user_game`, `user_roler`;
+DROP TABLE `character`, `game`, `game_category`, `resource`, `user`;
+DROP TABLE `country`, `gender`, `locale`;
+
 -- Tablas
 CREATE TABLE _sequence (
-    name      VARCHAR(100) PRIMARY KEY NOT NULL,
-    increment INT(11)                  NOT NULL DEFAULT 1,
-    min_value INT(11)                  NOT NULL DEFAULT 1,
-    max_value BIGINT(20)               NOT NULL DEFAULT 9223372036854775807,
-    cur_value BIGINT(20)                        DEFAULT 1,
-    cycle     BOOLEAN                  NOT NULL DEFAULT FALSE
+  name      VARCHAR(100) PRIMARY KEY NOT NULL,
+  increment INT(11)                  NOT NULL DEFAULT 1,
+  min_value INT(11)                  NOT NULL DEFAULT 1,
+  max_value BIGINT(20)               NOT NULL DEFAULT 9223372036854775807,
+  cur_value BIGINT(20)                        DEFAULT 1,
+  cycle     BOOLEAN                  NOT NULL DEFAULT FALSE
 );
 
-CREATE TABLE user (
-    idUser     INT(10) UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-    username   VARCHAR(40)           NOT NULL,
-    pass       VARCHAR(32)           NOT NULL,
-    email      VARCHAR(255)          NOT NULL,
-    name       VARCHAR(50)           NOT NULL,
-    lastname   VARCHAR(150),
-    idCountry  INT(10) UNSIGNED,
-    phone      VARCHAR(16),
-    birthdate  DATE                  NOT NULL,
-    idGender   INTEGER UNSIGNED,
-    isMaster   BOOLEAN               NOT NULL,
-    flagActive BOOLEAN DEFAULT TRUE  NOT NULL,
-    flagStatus BOOLEAN DEFAULT FALSE NOT NULL
+CREATE TABLE `user` (
+  idUser     INT(10) UNSIGNED PRIMARY KEY,
+  username   VARCHAR(40)           NOT NULL,
+  pass       VARCHAR(32)           NOT NULL,
+  email      VARCHAR(255)          NOT NULL,
+  name       VARCHAR(50)           NOT NULL,
+  lastname   VARCHAR(150),
+  idCountry  INT(10) UNSIGNED,
+  phone      VARCHAR(16),
+  birthdate  DATE                  NOT NULL,
+  idGender   INTEGER UNSIGNED,
+  isMaster   BOOLEAN               NOT NULL,
+  flagActive BOOLEAN DEFAULT TRUE  NOT NULL,
+  flagStatus BOOLEAN DEFAULT FALSE NOT NULL
 );
 CREATE UNIQUE INDEX user_username_uindex
-    ON user (username);
+  ON `user` (username);
 CREATE UNIQUE INDEX user_email_uindex
-    ON user (email);
-ALTER TABLE user
-    ADD INDEX (idCountry);
-ALTER TABLE user
-    ADD INDEX (idGender);
+  ON `user` (email);
+ALTER TABLE `user`
+  ADD INDEX (idCountry);
+ALTER TABLE `user`
+  ADD INDEX (idGender);
 
 CREATE TABLE user_roler (
-    idMaster INT(10) UNSIGNED,
-    idRoler  INT(10) UNSIGNED,
-    dateTime TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    CONSTRAINT userroler_pk PRIMARY KEY (idMaster, idRoler)
+  idMaster INT(10) UNSIGNED,
+  idRoler  INT(10) UNSIGNED,
+  dateTime TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  CONSTRAINT userroler_pk PRIMARY KEY (idMaster, idRoler)
 );
 
 CREATE TABLE country (
-    idCountry INT(10) UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-    code      VARCHAR(3) NOT NULL,
-    idLocale  INT(10) UNSIGNED
+  idCountry INT(10) UNSIGNED PRIMARY KEY,
+  code      VARCHAR(3) NOT NULL,
+  idLocale  INT(10) UNSIGNED
 );
 CREATE UNIQUE INDEX country_codeISO_uindex
-    ON country (code);
+  ON country (code);
 
 CREATE TABLE country_trans (
-    idTrans   INT(10) UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-    idCountry INT(10) UNSIGNED,
-    idLocale  INT(10) UNSIGNED,
-    text      VARCHAR(255) NOT NULL
+  idTrans   INT(10) UNSIGNED PRIMARY KEY,
+  idCountry INT(10) UNSIGNED,
+  idLocale  INT(10) UNSIGNED,
+  text      VARCHAR(255) NOT NULL
 );
 CREATE UNIQUE INDEX countrytrans_country_uindex
-    ON country_trans (idCountry, idLocale, text);
+  ON country_trans (idCountry, idLocale, text);
 
-CREATE TABLE localeGroup (
-    idLocale INT(10) UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-    codeISO  VARCHAR(3) NOT NULL
+CREATE TABLE locale (
+  idLocale INT(10) UNSIGNED PRIMARY KEY,
+  codeISO  VARCHAR(3) NOT NULL
 );
 CREATE UNIQUE INDEX locale_codeISO_uindex
-    ON localeGroup (codeISO);
+  ON locale (codeISO);
 
 CREATE TABLE locale_trans (
-    idTrans       INT(10) UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-    idLocale      INT(10) UNSIGNED,
-    idLocaleGroup INT(10) UNSIGNED,
-    text          VARCHAR(255) NOT NULL
+  idTrans       INT(10) UNSIGNED PRIMARY KEY,
+  idLocale      INT(10) UNSIGNED,
+  idLocaleGroup INT(10) UNSIGNED,
+  text          VARCHAR(255) NOT NULL
 );
 CREATE UNIQUE INDEX localetrans_locale_uindex
-    ON locale_trans (idLocale, idLocaleGroup, text);
+  ON locale_trans (idLocale, idLocaleGroup, text);
 
 CREATE TABLE resource (
-    idResource INT(10) UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-    code       VARCHAR(20) NOT NULL,
-    type       TINYINT(1)  NOT NULL,
-    file       TEXT        NOT NULL,
-    date       TIMESTAMP                    DEFAULT CURRENT_TIMESTAMP,
-    idUser     INT(10) UNSIGNED
+  idResource INT(10) UNSIGNED PRIMARY KEY,
+  code       VARCHAR(20) NOT NULL,
+  type       TINYINT(1)  NOT NULL,
+  file       TEXT        NOT NULL,
+  date       TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  idUser     INT(10) UNSIGNED
 );
 CREATE UNIQUE INDEX resource_code_uindex
-    ON resource (code);
+  ON resource (code);
 
 CREATE TABLE preference (
-    idPrefs INT(10) UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-    idUser  INT(10) UNSIGNED,
-    p_key   VARCHAR(20)  NOT NULL,
-    p_value VARCHAR(150) NOT NULL,
-    date    TIMESTAMP                    DEFAULT CURRENT_TIMESTAMP
+  idPrefs INT(10) UNSIGNED PRIMARY KEY,
+  idUser  INT(10) UNSIGNED,
+  p_key   VARCHAR(20)  NOT NULL,
+  p_value VARCHAR(150) NOT NULL,
+  date    TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 CREATE UNIQUE INDEX preference_idUser_uindex
-    ON preference (idUser, p_key);
+  ON preference (idUser, p_key);
 
 CREATE TABLE game_category (
-    idCategory INT(10) UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-    idUser     INT(10) UNSIGNED,
-    name       VARCHAR(200) NOT NULL
+  idCategory INT(10) UNSIGNED PRIMARY KEY,
+  idUser     INT(10) UNSIGNED,
+  name       VARCHAR(200) NOT NULL
 );
 CREATE UNIQUE INDEX game_category_name_uindex
-    ON game_category (name, idUser);
+  ON game_category (name, idUser);
 
 CREATE TABLE gender (
-    idGender INT(10) UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-    code     VARCHAR(3) NOT NULL
+  idGender INT(10) UNSIGNED PRIMARY KEY,
+  code     VARCHAR(3) NOT NULL
 );
 CREATE UNIQUE INDEX gender_code_uindex
-    ON gender (code);
+  ON gender (code);
 
 CREATE TABLE gender_trans (
-    idTrans  INT(10) UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-    idGender INT(10) UNSIGNED,
-    idLocale INT(10) UNSIGNED,
-    text     VARCHAR(255) NOT NULL
+  idTrans  INT(10) UNSIGNED PRIMARY KEY,
+  idGender INT(10) UNSIGNED,
+  idLocale INT(10) UNSIGNED,
+  text     VARCHAR(255) NOT NULL
 );
 CREATE UNIQUE INDEX gendertrans_gender_uindex
-    ON gender_trans (idGender, idLocale, text);
+  ON gender_trans (idGender, idLocale, text);
 
 CREATE TABLE parameter (
-    idParameter INT(10) UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-    category    VARCHAR(255) NOT NULL,
-    idLocale    INT(10) UNSIGNED,
-    text        VARCHAR(255) NOT NULL
+  idParameter INT(10) UNSIGNED PRIMARY KEY,
+  category    VARCHAR(255) NOT NULL,
+  idLocale    INT(10) UNSIGNED,
+  text        VARCHAR(255) NOT NULL
 );
 CREATE UNIQUE INDEX parameter_idTrans_uindex
-    ON parameter (idLocale, category, text);
+  ON parameter (idLocale, category, text);
 
 CREATE TABLE game (
-    idGame         INT(10) UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-    name           VARCHAR(100)                        NOT NULL,
-    code           VARCHAR(20)                         NOT NULL,
-    maxCharacters  INT(5)                              NOT NULL,
-    date           TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
-    zoneResource   INT(10) UNSIGNED                    NOT NULL,
-    headerResource INT(10) UNSIGNED                    NOT NULL,
-    status         TINYINT(1) DEFAULT 0                NOT NULL,
-    location       VARCHAR(255)                        NOT NULL,
-    idCategory     INT(10) UNSIGNED
+  idGame         INT(10) UNSIGNED PRIMARY KEY,
+  name           VARCHAR(100)                        NOT NULL,
+  code           VARCHAR(20)                         NOT NULL,
+  maxCharacters  INT(5)                              NOT NULL,
+  date           TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+  zoneResource   INT(10) UNSIGNED                    NOT NULL,
+  headerResource INT(10) UNSIGNED                    NOT NULL,
+  status         TINYINT(1) DEFAULT 0                NOT NULL,
+  location       VARCHAR(255)                        NOT NULL,
+  idCategory     INT(10) UNSIGNED
 );
 CREATE UNIQUE INDEX game_name_uindex
-    ON game (name);
+  ON game (name);
 CREATE UNIQUE INDEX game_code_uindex
-    ON game (code);
+  ON game (code);
 
 CREATE TABLE user_game (
-    idGame INT(10) UNSIGNED NOT NULL,
-    idUser INT(10) UNSIGNED NOT NULL,
-    date   TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    CONSTRAINT usergame_pk PRIMARY KEY (idGame, idUser)
+  idGame INT(10) UNSIGNED NOT NULL,
+  idUser INT(10) UNSIGNED NOT NULL,
+  date   TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  CONSTRAINT usergame_pk PRIMARY KEY (idGame, idUser)
 );
 
 CREATE TABLE game_resource (
-    idResource INT(10) UNSIGNED NOT NULL,
-    idGame     INT(10) UNSIGNED NOT NULL,
-    date       TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    CONSTRAINT gameresource_pk PRIMARY KEY (idGame, idResource)
+  idResource INT(10) UNSIGNED NOT NULL,
+  idGame     INT(10) UNSIGNED NOT NULL,
+  date       TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  CONSTRAINT gameresource_pk PRIMARY KEY (idGame, idResource)
 );
 
 CREATE TABLE `character` (
-    idCharacter INT(10) UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-    name        VARCHAR(255) NOT NULL,
-    surname     VARCHAR(255) NOT NULL,
-    lastname    VARCHAR(255) NOT NULL,
-    birthdate   DATE         NOT NULL,
-    planet      VARCHAR(255) NOT NULL,
-    idGender    INT(10) UNSIGNED,
-    chronicle   VARCHAR(255) NOT NULL,
-    behaviour   INT(10)      NOT NULL,
-    `character` INT(10)      NOT NULL,
-    concept     INT(10)      NOT NULL,
-    humanity    INT(10)      NOT NULL        DEFAULT 1,
-    will        INT(10)      NOT NULL        DEFAULT 1,
-    faith       INT(10)      NOT NULL        DEFAULT 1,
-    health      INT(10)      NOT NULL        DEFAULT 1,
-    experience  VARCHAR(255) NULL
+  idCharacter INT(10) UNSIGNED PRIMARY KEY,
+  name        VARCHAR(255) NOT NULL,
+  surname     VARCHAR(255) NOT NULL,
+  lastname    VARCHAR(255) NOT NULL,
+  birthdate   DATE         NOT NULL,
+  planet      VARCHAR(255) NOT NULL,
+  idGender    INT(10) UNSIGNED,
+  chronicle   VARCHAR(255) NOT NULL,
+  behaviour   INT(10)      NOT NULL,
+  `character` INT(10)      NOT NULL,
+  concept     INT(10)      NOT NULL,
+  humanity    INT(10)      NOT NULL        DEFAULT 1,
+  will        INT(10)      NOT NULL        DEFAULT 1,
+  faith       INT(10)      NOT NULL        DEFAULT 1,
+  health      INT(10)      NOT NULL        DEFAULT 1,
+  experience  VARCHAR(255) NULL
 );
 
 CREATE TABLE character_game (
-    idGame      INT(10) UNSIGNED NOT NULL,
-    idCharacter INT(10) UNSIGNED NOT NULL,
-    date        TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    CONSTRAINT charactergame_pk PRIMARY KEY (idGame, idCharacter)
+  idGame      INT(10) UNSIGNED NOT NULL,
+  idCharacter INT(10) UNSIGNED NOT NULL,
+  date        TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  CONSTRAINT charactergame_pk PRIMARY KEY (idGame, idCharacter)
 );
 
 CREATE TABLE character_user (
-    idUser      INT(10) UNSIGNED NOT NULL,
-    idCharacter INT(10) UNSIGNED NOT NULL,
-    date        TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    CONSTRAINT charactergame_pk PRIMARY KEY (idUser, idCharacter)
+  idUser      INT(10) UNSIGNED NOT NULL,
+  idCharacter INT(10) UNSIGNED NOT NULL,
+  date        TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  CONSTRAINT charactergame_pk PRIMARY KEY (idUser, idCharacter)
 );
