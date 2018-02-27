37f02276a3c177af8587ab4fa793a5f9b141195e
pedro oliva gil <onion_oliva@hotmail.com>
2018-02-27 18:54:34 +0100

    V.0.0.50 Entities Prueba

--- db/testData.sql
+++ db/testData.sql
@@ -1,5 +1,19 @@
 INSERT INTO locale (idLocale, codeISO) VALUES ((SELECT nextval('locale')), 'EN');
 
-select * from locale;
+INSERT INTO locale_trans (idTrans, idLocale, idLocaleGroup, text)
+VALUES ((SELECT nextval('locale')), 142, 142, 'Español');
 
-SELECT * FROM locale WHERE codeISO = 'ES' AND idLocale = 142;
+SELECT *
+FROM locale;
+
+SELECT *
+FROM locale
+WHERE codeISO = 'ES' AND idLocale = 142;
+
+SELECT *
+FROM locale t1
+WHERE t1.codeISO = 'ES' AND t1.idLocale = 142;
+
+SELECT *
+FROM locale t1 LEFT JOIN locale_trans t2 ON t1.idLocale = t2.idLocaleGroup
+WHERE t1.codeISO = 'ES' AND t1.idLocale = 142;
